5745ba393ed3580745e1b38f085be4479d96e521
Tomas Roun <tomas.roun@cern.ch>
2023-07-10 20:06:21 +0200

    WIP

--- public/main.js
+++ public/main.js
@@ -4,30 +4,12 @@ document.addEventListener("DOMContentLoaded", (e) => {
     ".timeline"
   ).style.maxHeight = `calc(90vh - ${timeline.offsetTop}px)`;
 
-  [...document.querySelectorAll(".issues button")].forEach((elem) => {
+  [...document.querySelectorAll(".issues .issue button")].forEach((elem) => {
     elem.addEventListener("click", (e) => {
-      const issue = parseInt(e.target.dataset.issue, 10);
+      const issue = parseInt(e.target.textContent, 10);
       document.querySelector(
         ".viewer object"
       ).data = `issues/0${issue}-1965.pdf`;
-    });
-  });
-
-  [...document.querySelectorAll(".year button")].forEach((elem) => {
-    elem.addEventListener("click", (e) => {
-      const issues =
-        elem.parentElement.parentElement.nextElementSibling.querySelector(
-          ".issues"
-        );
-      console.log(issues);
-      const height = issues.style.height;
-      console.log("height", height);
-      if (height && height !== "0px") {
-        issues.style.height = 0;
-      } else {
-        console.log(`${issues.scrollHeight}px`);
-        issues.style.height = `${issues.scrollHeight}px`;
-      }
     });
   });
 });
